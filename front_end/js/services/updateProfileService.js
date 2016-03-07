8366fc4ddd97bb1ff85db8374166e511859bdef8
YeeunYou <emilyyou95@gmail.com>
2016-03-07 14:05:24 -0500

    basic profile layout

---
+++ front_end/js/services/updateProfileService.js
@@ -0,0 +1,13 @@
+var app = angular.module("HueMeApp");
+app.service("updateProfileService", function($http, $localStorage) { 
+	this.updateUser = function(userData, callback){ 
+		$http.post('',{'user': userData })
+		.then(function(response) {
+			var user = JSON.parse(response.data);
+			$localStorage.user = user; 
+			callback(true);
+		}, function(error) {
+			callback(false);
+		});
+	}
+})
