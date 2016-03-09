d719835655b987b4044bb8bd5059854b90ada4f4
Infiniteloop82 <infiniteloop82@outlook.com>
2016-03-09 03:11:21 -0500

    new models

--- application/models/MemberModel.php
+++ application/models/MemberModel.php
@@ -4,19 +4,27 @@ class MemberModel extends CI_Model {
 
     function registerUser($register) {
         $json_reg = $register; // json_decode($register);
+        if(!$userExists($json_reg['email'], $json_reg['username'])){
 
-        $qr = 'INSERT INTO users (username, password, email) VALUES ("' . $json_reg['username'] . '", "' . $json_reg['password'] . '", "' . $json_reg['email'] . '")';
+            $qr = 'INSERT INTO users (username, password, email) VALUES ("' . $json_reg['username'] . '", "' . $json_reg['password'] . '", "' . $json_reg['email'] . '")';
 
-        $rs = $this->db->query($qr);
+            $rs = $this->db->query($qr);
 
         //this is the same as inser_id in php
-        $id = $this->db->insert_id();
+            $id = $this->db->insert_id();
 
-        if ($rs) {
-            return $this->getProfile($id);
-        } else {
-            return http_response_code(500);
+            if ($rs) {
+                return $this->getProfile($id);
+            } else {
+                return http_response_code(409);
+            }
+        }else{
+            return json_encode("{
+                'username' : 'true',
+                'email' : 'true'
+            }");
         }
+
     }
 
     function loginUser($user) {
@@ -37,29 +45,34 @@ class MemberModel extends CI_Model {
 
     public function updateProfile($user) {
 
-        $query = "UPDATE users SET photo_url= " . $user['photo_url'] . " ,dob = " . $user['dob'] .
-                " ,description= " . $user['description'] . " ,gender=" . $user['gender'] .
-                ", status= " . $user['description'] . ",country=" . $user['country'] . ",region" .
-                $user['description'] . ",city=" . $user['description'] . ",zip_code=" . $user['description'] .
-                "mood_colours_id=" . $user['description'] . " WHERE sessionid = " . $user['sessionid'];
+        $query = "UPDATE users SET photo_url= '" . $user['photo_url'] . "', dob = '" . $user['dob'] .
+        "' ,description= '" . $user['description'] . "' ,gender='" . $user['gender'] .
+        "', status= '" . $user['description'] . "',country='" . $user['country'] . "',region='" .
+        $user['region'] . "',city='" . $user['city'] . "',zip_code='" . $user['zipcode'] .
+        "', mood_colours_id=" . $user['colour'] . " WHERE email = '" . $user['email'] . "'";
         $rs = $this->db->query($query); //gets the data from the table
         return json_encode($rs->result_array());
     }
 
     private function getProfile($id) {
-        $qry = 'SELECT dob, description, gender, status, country, region, city,zip_code, photo_url FROM users WHERE id = ' . $id;
+        $qry = 'SELECT email, dob, description, gender, status, country, region, city,zip_code, photo_url FROM users WHERE id = ' . $id;
         $userInfo = $this->db->query($qry);
         $user = $userInfo->result_array();
         return json_encode($user);
     }
 
-}
+    public function usernameExists($username){
+        $qry = "SELECT id FROM users WHERE username = '" . $username . "'";
+        $id = $this->db->query($qry);
+       $row = $id->result_array();
+        return json_encode($row); //) ? "true" : "false"); 
+    }
 
-/*
- *
- * 
- * 
- * 
- * creat sessionid and insert into database and check ajax for session 
- * 
- */
+    public function useremailExists($email){
+        $qry = "SELECT id FROM users WHERE email = '" . $email . "'";
+        $id = $this->db->query($qry);
+        $row = $id->result_array();
+        return json_encode($row); //) ? "true" : "false");        
+    }
+
+}
--- application/models/PostsModel.php
+++ application/models/PostsModel.php
@@ -1,12 +1,38 @@
 <?php
 
-class Posts_Model extends CI_Model{
-    
-    public function getPosts($returnNum){
+class PostsModel extends CI_Model{
+
+	public function getPosts($returnNum){
         //$query = "SELECT * FROM posts  ORDER BY create_time  DESC LIMIT " . $returnNum;
-        $query = 'SELECT p.content "post", p.create_time "time", u.username "user", mc.colour "colour" FROM posts p, users u, mood_colours mc WHERE p.users_id = u.id AND p.mood_colours_id = mc.id ORDER BY p.create_time DESC LIMIT ' . $returnNum;
+		$query = 'SELECT p.content "post", p.create_time "time", u.username "user", mc.colour "colour" FROM posts p, users u, mood_colours mc WHERE p.users_id = u.id AND p.mood_colours_id = mc.id ORDER BY p.create_time DESC LIMIT ' . $returnNum;
         $rs = $this->db->query($query); //gets the data from the table
-       return json_encode($rs->result_array());
+        return json_encode($rs->result_array());
         //return $rs->result_array();
     }
-}
+
+    public function insertPosts($post){
+    	$userId = $this->getUserIdByEmail($post['email']);
+		$qry = "INSERT INTO posts (users_id, mood_colours_id, tags, content) VALUES (" .
+		$userId[0]['id'] . ", " . $post['mood_colours_id'] . ", '" . $post['tags'] .  
+		"', '" . $post['content'] . "')"; 
+		$rtn = $this->db->query($qry);
+		$id = $this->db->insert_id();
+		return json_encode($id);
+	}
+
+	public function insertComment($comment){
+		$userId = $this->getUserIdByEmail($comment['email']);
+		$qry = "INSERT INTO comments (users_id, posts_id, content) VALUES (" .
+		$userId[0]['id'] . ", " . $comment['postId'] .  ", '" . $comment['content'] . "')"; 
+		$rtn = $this->db->query($qry);
+		$id = $this->db->insert_id();
+		return json_encode($id);
+	}
+
+	private function getUserIdByEmail($email){		
+		$qry = "SELECT id FROM users WHERE email = '" . $email . "'";
+		$id = $this->db->query($qry);
+		$row = $id->result_array();
+	     return $row; //) ? "true" : "false");        
+	    }
+	}
