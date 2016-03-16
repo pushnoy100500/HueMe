d7c5f360da1730f465aac68d4fe3d827d685790a
jjbruce <josh.bruce@gmail.com>
2016-03-15 22:36:08 -0400

    Profile page updates, minor styling changes - Added ui-select, and sanitize angular modules - Changed profile page styles, and submission forms - Minor style changes to landing

--- front_end/js/app.js
+++ front_end/js/app.js
@@ -1,5 +1,5 @@
 'use strict'
-var app = angular.module("HueMeApp", ['ui.router', 'ui.bootstrap', 'jcs-autoValidate', 'ngStorage']);
+var app = angular.module("HueMeApp", ['ui.router', 'ui.bootstrap', 'jcs-autoValidate', 'ngStorage', 'ui.select', 'ngSanitize']);
 
 //router definition
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -18,12 +18,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
 					var deferred = $q.defer();
 					$http({method: 'GET', url: 'http://localhost:8888/hueme/'})
 						.then(function(data) {
-							var temp = JSON.parse(data.data);
-							temp = temp.map(function(post) {
-								var post = post;
+							//var temp = JSON.parse(data.data);
+							var	temp = $.map(data, function(post) {
+								//var post = post;
 								post.time = timeSinceService.timeSince(new Date(post.time));
 								return post;
-							})
+							});
 							deferred.resolve(temp);
 						});
 						return deferred.promise;
@@ -45,7 +45,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		.state('myprofile', {
 			url: "/myprofile",
 			template: "<my-profile-dir></my-profile-dir>"
-		})
+		});
 });
 
 app.run(
@@ -71,6 +71,6 @@ app.run(
 							$location.path(toState.url);
 					}
 					/*   */
-				})
+				});
     }
 );
--- front_end/js/directives/myProfileDir.js
+++ front_end/js/directives/myProfileDir.js
@@ -1,47 +1,17 @@
 var app = angular.module("HueMeApp");
 
-app.directive('myProfileDir', function($localStorage, $state, regLogService, updateProfileService, avatarService, countryService) {
+app.directive('myProfileDir', function($localStorage, $state, regLogService) {
   return {
     restrict: "E",
     templateUrl: "templates/myProfile.html",
     controller: function() {
-      this.user = $localStorage.user[0];
+      this.user = $localStorage.user;
       this.logOut = function() {
         regLogService.logOut(function() {
           $state.go('landing');
         })
       }
-      $localStorage.userTemp = $localStorage.user[0];
-
-      this.save = function(){
-         alert('save');
-       }
-       this.avatars = avatarService.avatars;
-
-      this.countries = countryService.countries;
-
-      this.temp = {};
-
-      this.avatSel = function(index){
-        this.temp.photo_url = this.avatars[index].url;
-        this.selected = index + 1;
-      }
-
-      this.genders = ['Female', 'Male', 'Other'];
-      this.editorEnabled = false;
-
-      this.enableEditor = function(){
-        this.editorEnabled = true;
-        this.temp = jQuery.extend({}, this.user);
-      }
-
-      this.disableEditor = function(){
-        this.editorEnabled = false;
-        this.selected = '';
-        this.temp = {};
-      }
     },
-
     controllerAs: "profileCtrl"
 
   }
