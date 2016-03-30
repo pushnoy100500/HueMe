cdb62605b2c7b796aa62f36493b60f06e436af9d
Sereneryo <Sereneryo@gmail.com>
2016-03-30 12:43:27 -0400

    trash these commits

--- front_end/js/services/urlService.js
+++ front_end/js/services/urlService.js
@@ -1,10 +1,10 @@
 var app = angular.module("HueMeApp");
 app.service('urlService', function() {
-	this.recentPostsUrl = "http://localhost:8888/hueme/";
-	this.loginUrl = "http://localhost:8888/hueme/login";
-	this.registerUrl = "http://localhost:8888/hueme/register";
-	this.updateProfUrl = "http://localhost:8888/hueme/updateProfile";
-	this.leavePostUrl = "http://localhost:8888/hueme/post";
-	this.postsByUserUrl = "http://localhost:8888/hueme/postsByUser"
-	this.aboutUrl = "http://localhost:8888/hueme/about"
+	this.recentPostsUrl = "http://127.0.0.1:8888/HueMe/front_end/";
+	this.loginUrl = "http://127.0.0.1:8888/HueMefront_end//login";
+	this.registerUrl = "http://127.0.0.1:8888/HueMe/front_end/register";
+	this.updateProfUrl = "http://127.0.0.1:8888/HueMe/front_end/updateProfile";
+	this.leavePostUrl = "http://127.0.0.1:8888/HueMe/front_end/post";
+	this.postsByUserUrl = "http://127.0.0.1:8888/HueMe/front_end/postsByUser"
+	this.aboutUrl = "http://127.0.0.1:8888/HueMe/front_end/about"
 })
